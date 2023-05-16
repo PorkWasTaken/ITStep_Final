71f65b50e65dff3cf4d0902db0a27415ad604a64
PorkWasTaken <chaxoxbilijonjoli2018@gmail.com>
2023-05-16 10:37:08 +0100

    Login Page

---
+++ Login.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".login").hide();
+    $(".register_li").addClass("active");
+    $(".login_li").click(function(){
+        $(this).addClass("active")
+        $(".register_li").removeClass("active");
+        $(".login").show();
+        $(".register").hide();
+    })
+    $(".register_li").click(function(){
+        $(this).addClass("active")
+        $(".login_li").removeClass("active");
+        $(".login").hide();
+        $(".register").show();
+    })
+});
