a99309a9f94ded4f702ed9d3300c0e105b9574df
PorkWasTaken <chaxoxbilijonjoli2018@gmail.com>
2023-05-20 11:48:20 +0100

    Merge branch 'main' of https://github.com/PorkWasTaken/ITStep_Final

--- Login.js
+++ Login.js
@@ -14,3 +14,61 @@ $(document).ready(function(){
         $(".register").show();
     })
 });
+
+$(document).ready(function() {
+    $('#BTNN').submit(function(event) {
+      event.preventDefault();
+  
+      var email = $('#email-input').val();
+      var username = $('#username-input').val();
+      var password = $('#password-input').val();
+  
+      var userData = {
+        email: email,
+        username: username,
+        password: password
+      };
+  
+      $.ajax({
+        url: 'http://kketelauri-001-site1.gtempurl.com/api/user/adduser',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(userData),
+        contentType: 'application/json',
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+        }
+      });
+    });
+  
+    $('#Login').submit(function(event) {
+      event.preventDefault();
+  
+      var email = $('#email-login-input').val();
+      var password = $('#password-login-input').val();
+  
+      var loginData = {
+        email: email,
+        password: password
+      };
+  
+      $.ajax({
+        url: 'http://kketelauri-001-site1.gtempurl.com/api/user/login',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(loginData),
+        contentType: 'application/json',
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+        }
+      });
+    });
+  });
+  
+  
